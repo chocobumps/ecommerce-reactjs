7c0217a7242b8d5c7f4d8b359772d1b743d0ee78
kath <kath.pineds@gmail.com>
2023-09-28 10:53:48 +0800

    PS-01 - created images, screens folders - created HomeScreen.js file

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import products from '../products';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row></Row>
+    </>
+  );
+};
+
+export default HomeScreen;
