62e3855c68e2474cf963b4d8feeea290d2df4f60
kath <kath.pineds@gmail.com>
2023-10-13 10:18:54 +0800

    PS-01 - imported Link from react-router-dom - changed all a href tags to Link to

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,19 +7,25 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
               {/* ms : margin from start (start is left = ml in bootstrap 4). me : margin from end (end is right = mr in bootstrap 4) */}
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart pe-1'></i>
-                Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user pe-1'></i>
-                Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart pe-1'></i>
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user pe-1'></i>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
