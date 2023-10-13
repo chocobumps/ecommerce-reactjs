9270af80eaf9faeca5242ac0be924de921f02f5c
kath <kath.pineds@gmail.com>
2023-10-13 09:38:55 +0800

    PS-01 - used BrowserRouter - added paths to HomeScreen and ProductScreen - created ProducScreen.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            {/* 'exact' makes sure that it only returns the route if the path is an EXACT match to the current url, in this case '/HomeScreen' */}
+            <Route path='/product/:id' element={<ProductScreen />} />
+            {/* :id is an id param that is used as a placeholder */}
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
